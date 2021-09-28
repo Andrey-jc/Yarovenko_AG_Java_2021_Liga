9b0897a1c63918dc281c251672c2e9de2da60a37
Andrey-jc <stuffingcode@gmail.com>
2021-09-28 18:16:47 +0300

    changed sql script

--- Task5/V1__init_schema.sql
+++ Task5/V1__init_schema.sql
@@ -1,58 +1,56 @@
-create table teacher
+create table TEACHER
 (
     id         integer primary key,
     patronymic varchar(255),
-    firstname  varchar(255),
+    name       varchar(255),
     surname    varchar(255),
     age        integer not null,
     sex        varchar(5),
     school_id  integer
 );
 
-create table student
+create table STUDENT
 (
     id         integer primary key,
     patronymic varchar(255),
-    firstname  varchar(255),
+    name       varchar(255),
     surname    varchar(255),
     age        integer not null,
     sex        varchar(5),
     school_id  integer
 );
 
-create table school
+create table SCHOOL
 (
     id      integer primary key,
     name    varchar(255),
     address varchar(255)
 );
 
-create table lesson
+create table LESSON
 (
     id   integer primary key,
     name varchar(255)
 );
 
-create table studentAndLesson
+create table STUDENT_AND_LESSON
 (
-    id         integer primary key,
     student_id integer,
-    lesson_id  integer,
-    foreign key (lesson_id) references lesson (id),
-    foreign key (student_id) references student (id)
+    lesson_id  integer NOT NULL UNIQUE,
+    foreign key (lesson_id) references LESSON (id),
+    foreign key (student_id) references STUDENT (id)
 );
 
-create table teacherAndLesson
+create table TEACHER_AND_LESSON
 (
-    id         integer primary key,
     teacher_id integer,
-    lesson_id  integer,
-    foreign key (teacher_id) references teacher (id),
-    foreign key (lesson_id) references lesson (id)
+    lesson_id  integer NOT NULL UNIQUE,
+    foreign key (teacher_id) references TEACHER (id),
+    foreign key (lesson_id) references LESSON (id)
 );
 
 alter table teacher
-    add foreign key (school_id) references school (id);
+    add foreign key (school_id) references SCHOOL (id);
 
 alter table student
-    add foreign key (school_id) references school (id);
+    add foreign key (school_id) references SCHOOL (id);
